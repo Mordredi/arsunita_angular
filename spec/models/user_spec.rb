dc1140383ac2742c8f2d79ac9d1d9b56c06e16a7
Arthur <arthur.p.wright@gmail.com>
2015-08-22 18:01:42 -0400

    Changed test a bit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "correctly sets the users age" do
-    user = create(:user, birthday: Time.new(1984, 04, 13))
+    user = create(:user, birthday: "1984-04-13")
     expect(user.age).to eq 31
   end
 end
