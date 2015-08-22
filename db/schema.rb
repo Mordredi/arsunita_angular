4f55945fda3a9d39884e06fc95a560dca6d8935b
Arthur <arthur.p.wright@gmail.com>
2015-08-22 10:38:46 -0400

    Generated venue model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822143424) do
+ActiveRecord::Schema.define(version: 20150822143757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,14 @@ ActiveRecord::Schema.define(version: 20150822143424) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
+  create_table "venues", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "capacity"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   add_foreign_key "shows", "events"
   add_foreign_key "subscriptions", "users"
---
+++ spec/factories/venues.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :venue do
+    name "MyString"
+description "MyText"
+capacity 1
+  end
+
+end
