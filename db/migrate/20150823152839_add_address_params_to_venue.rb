b1aeaad54b02405a33f375869556dd547e1996ef
Arthur <arthur.p.wright@gmail.com>
2015-08-23 11:29:17 -0400

    Added address columns to venue

---
+++ db/migrate/20150823152839_add_address_params_to_venue.rb
@@ -0,0 +1,8 @@
+class AddAddressParamsToVenue < ActiveRecord::Migration
+  def change
+    add_column :venues, :address, :string
+    add_column :venues, :city, :string
+    add_column :venues, :province, :string
+    add_column :venues, :country, :string
+  end
+end
