45b97f28153d9a2cffe7b7ba9aea4b12b4342532
Arthur <arthur.p.wright@gmail.com>
2015-08-23 11:27:01 -0400

    Added longitude and latitude to venues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823130948) do
+ActiveRecord::Schema.define(version: 20150823152612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,8 @@ ActiveRecord::Schema.define(version: 20150823130948) do
     t.integer  "capacity"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_foreign_key "events", "venues"
